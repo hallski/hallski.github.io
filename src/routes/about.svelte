7fe9d3b3648ac2b257a3d099506b31bdd8395abb
Mikael Hallendal <hallski@hallski.org>
2021-10-31 11:24:45 +0100

    Time to test

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -7,5 +7,5 @@
 	Personal blog about development, architecture and other code related topics. I have a strong
 	interest in learning new languages and explore various aspects of programming. Currently I'm
 	working primarily in Javascript on Google Cloud Platform but have in the past worked 8 years as an
-	iOS developer and prior to that about 10 years of GNOME/Linux development.
+	iOS developer and prior to that about 10 years of GNOME/Linux development. S
 </p>
