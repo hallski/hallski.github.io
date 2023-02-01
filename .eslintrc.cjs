4818894980df880c27f1eff4e5efc82b57a4eee1
Mikael Hallendal <hallski@hallski.org>
2023-02-02 00:02:18 +0100

    Update lint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,17 @@
 module.exports = {
 	root: true,
-	extends: ['eslint:recommended'],
-	overrides: [
-		{ files: ['*.svelte'], processor: 'svelte3/svelte3' },
-		{
-			files: ['*.ts'],
-			parser: '@typescript-eslint/parser',
-			extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-			plugins: ['svelte3', '@typescript-eslint'],
-			settings: {
-				'svelte3/typescript': () => require('typescript')
-			}
-		}
-	],
+	parser: '@typescript-eslint/parser',
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	plugins: ['svelte3', '@typescript-eslint'],
+	ignorePatterns: ['*.cjs', 'build/*'],
+	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	settings: {
+		'svelte3/typescript': () => require('typescript')
+	},
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2019
+		ecmaVersion: 2020
 	},
-	ignorePatterns: ['*.cjs'],
 	env: {
 		browser: true,
 		es2017: true,
