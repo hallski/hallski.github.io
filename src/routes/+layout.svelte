5671a4e45fc79fff1ee1d8bbcf6a23e46aeb44c7
Mikael Hallendal <hallski@hallski.org>
2023-02-02 00:27:57 +0100

    Don't move atom feed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <svelte:head>
-	<link rel="alternate" type="application/atom+xml" href="https://hallski.org/feed/atom" />
+	<link rel="alternate" type="application/atom+xml" href="https://hallski.org/atom.xml" />
 	<link rel="alternate" type="application/rss+xml" href="https://hallski.org/feed/rss" />
 	<link rel="alternate" type="application/json" href="https://hallski.org/feed/json" />
 	<link rel="stylesheet" href="/css/style.css" type="text/css" />
